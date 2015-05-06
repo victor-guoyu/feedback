a99816ed68be63c3b446f608a979e65be47bb86e
unknown <guoy@CA628188.mitel.com>
2015-05-06 15:22:32 -0400

    npm & bower init

--- public/js/app.js
+++ public/js/app.js
@@ -1,65 +1,54 @@
-        // Include the UserVoice JavaScript SDK (only needed once on a page)
-        UserVoice = window.UserVoice || [];
-        (function ()
-        {
-            var uv = document.createElement('script');
-            uv.type = 'text/javascript';
-            uv.async = true;
-            uv.src = '//widget.uservoice.com/sUmr3y8NMT63V1h8DHTJxw.js';
-            var s = document.getElementsByTagName('script')[0];
-            s.parentNode.insertBefore(uv, s)
-        })();
+UserVoice = window.UserVoice || [];
 
-        //
-        // UserVoice Javascript SDK developer documentation:
-        // https://www.uservoice.com/o/javascript-sdk
-        //
+/**
+ * Identify the user and pass traits
+ * To enable, replace sample data with actual user traits and uncomment the line
+ */
+UserVoice.push([
+    'identify', {
+        //email:      'john.doe@example.com', // User’s email address
+        //name:       'John Doe', // User’s real name
+        //created_at: 1364406966, // Unix timestamp for the date the user signed up
+        //id:         123, // Optional: Unique id of the user (if set, this should not change)
+        //type:       'Owner', // Optional: segment your users by type
+        //account: {
+        //  id:           123, // Optional: associate multiple users with a single account
+        //  name:         'Acme, Co.', // Account name
+        //  created_at:   1364406966, // Unix timestamp for the date the account was created
+        //  monthly_rate: 9.99, // Decimal; monthly rate of the account
+        //  ltv:          1495.00, // Decimal; lifetime value of the account
+        //  plan:         'Enhanced' // Plan name for the account
+        //}
 
-        // Identify the user and pass traits
-        // To enable, replace sample data with actual user traits and uncomment the line
-        UserVoice.push([
-            'identify', {
-                //email:      'john.doe@example.com', // User’s email address
-                //name:       'John Doe', // User’s real name
-                //created_at: 1364406966, // Unix timestamp for the date the user signed up
-                //id:         123, // Optional: Unique id of the user (if set, this should not change)
-                //type:       'Owner', // Optional: segment your users by type
-                //account: {
-                //  id:           123, // Optional: associate multiple users with a single account
-                //  name:         'Acme, Co.', // Account name
-                //  created_at:   1364406966, // Unix timestamp for the date the account was created
-                //  monthly_rate: 9.99, // Decimal; monthly rate of the account
-                //  ltv:          1495.00, // Decimal; lifetime value of the account
-                //  plan:         'Enhanced' // Plan name for the account
-                //}
+    }
+]);
 
-            }
-        ]);
+// Set global widget options
+UserVoice.push([
+    'set', {
+        mode: 'satisfaction',
+        accent_color: '#448dd6',
+        trigger_color: 'white',
+        trigger_background_color: '#448dd6',
+    }
+]);
 
-        // Set global widget options
-        UserVoice.push([
-            'set', {
-                mode: 'satisfaction',
-                accent_color: '#448dd6',
-                trigger_color: 'white',
-                trigger_background_color: '#448dd6',
-            }
-        ]);
-
-        UserVoice.push(['addTrigger']);
-        UserVoice.push(['show']);
+UserVoice.push(['addTrigger']);
+UserVoice.push(['show']);
 		
-		UserVoice.push(['embed', '#SmartVoteEmbedWidget', {
+UserVoice.push(['embed', '#SmartVoteEmbedWidget', {
   mode: 'smartvote',
   forum_id: '289798',
   height: '325px',
   width: '325px'
 }]);
+
 UserVoice.push(['embed', '#ContactEmbedWidget', {
   mode: 'contact',
   height: '325px',
   width: '325px'
 }]);
+
 UserVoice.push(['embed', '#SatisfactionEmbedWidget', {
   mode: 'satisfaction',
   height: '325px',
