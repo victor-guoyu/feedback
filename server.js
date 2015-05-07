6d089c8197a2a011bef36a837c20ddec0095adf6
Victor Guo <victor.guoyu@hotmail.com>
2015-05-06 22:47:41 -0400

    Fixed bower dependency

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 var express = require('express');
 var server = express();
 
-var port = 8080,
-	dir = '/public';
+var port = 8080 || process.env.PORT,
+    dir = '/public';
 
 server.use(express.static(__dirname+'/public'));
+server.use('/bower_components', express.static(__dirname+'/bower_components'));
 
-server.listen(8080);
-
-console.log('Server running at port '+port+ ' & serving '+ dir);
+server.listen(port, function(){
+  console.log('Server running at port '+port+ ' & serving '+ dir);
+});
